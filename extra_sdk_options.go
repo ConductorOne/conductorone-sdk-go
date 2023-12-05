72ebdc7e9600a75a09127ba036666bcac8cd762b
Anthony Zhao <anthony.zhao@conductorone.com>
2023-12-05 18:24:16 -0500

    Added tests

--- extra_sdk_options.go
+++ extra_sdk_options.go
@@ -3,7 +3,10 @@ package conductoronesdkgo
 import (
 	"context"
 	"crypto/tls"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"go.uber.org/zap"
 
@@ -62,12 +65,26 @@ func WithTLSConfig(tlsConfig *tls.Config) CustomSDKOption {
 	}
 }
 
-type CustomOptions struct {
+type ClientConfig struct {
 	useWithServer bool
 	useWithTenant bool
 
 	ServerURL string
 	Tenant    string
+}
+
+func (c ClientConfig) GetServerURL() string {
+	if c.useWithServer {
+		return c.ServerURL
+	}
+	if c.useWithTenant {
+		return fmt.Sprintf("https://%s.conductor.one", c.Tenant)
+	}
+	return ""
+}
+
+type CustomOptions struct {
+	ClientConfig
 
 	withClient *http.Client
 	logger     *zap.Logger
@@ -83,14 +100,17 @@ func NewWithCredentials(ctx context.Context, cred *ClientCredentials, opts ...Cu
 	}
 
 	var serverURL string
-	if options.ServerURL != "" {
-		serverURL = options.ServerURL
+	if options.GetServerURL() != "" {
+		serverURL = options.GetServerURL()
 	} else {
-		resp, err := NormalizeTenant(cred.ClientID)
+		resp, err := ParseClientID(cred.ClientID)
 		if err != nil {
 			return nil, err
 		}
-		serverURL = resp.ServerURL
+		serverURL = resp.GetServerURL()
+		if serverURL == "" {
+			return nil, ErrInvalidClientID
+		}
 	}
 
 	tokenSource, err := NewTokenSource(ctx, cred.ClientID, cred.ClientSecret, serverURL)
@@ -123,4 +143,58 @@ func NewWithCredentials(ctx context.Context, cred *ClientCredentials, opts ...Cu
 	sdkOpts = append(sdkOpts, WithClient(uclient))
 
 	return New(sdkOpts...), nil
+}
+
+func NormalizeTenant(input string) (*ClientConfig, error) {
+	input = strings.ToLower(input)
+
+	var err error
+	u := &url.URL{}
+	if !strings.Contains(input, "//") {
+		if !strings.Contains(input, ".") {
+			input += ".conductor.one"
+		}
+		u.Host = input
+	} else {
+		u, err = url.Parse(input)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	normalize := &ClientConfig{}
+
+	parts := strings.Split(u.Host, ".")
+	if len(parts) == 3 && parts[1] == "conductor" && parts[2] == "one" {
+		normalize.useWithTenant = true
+		normalize.Tenant = parts[0]
+
+		return normalize, nil
+	}
+
+	u.Scheme = "https"
+	normalize.useWithServer = true
+	normalize.ServerURL = u.String()
+	return normalize, nil
+}
+
+func ParseClientID(input string) (*ClientConfig, error) {
+	// split the input into 2 parts by @
+	items := strings.SplitN(input, "@", 2)
+	if len(items) != 2 {
+		return nil, ErrInvalidClientID
+	}
+
+	// split the right part into 2 parts by /
+	items = strings.SplitN(items[1], "/", 2)
+	if len(items) != 2 {
+		return nil, ErrInvalidClientID
+	}
+
+	resp, err := NormalizeTenant(items[0])
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
--- extra_sdk_options_test.go
+++ extra_sdk_options_test.go
@@ -38,21 +38,21 @@ func TestNormalizeTenantInput(t *testing.T) {
 			name:               "differentUrlWithHTTP",
 			input:              "https://insulator.a.different.com",
 			wantedURL:          "https://insulator.a.different.com",
-			wantedTenantDomain: "invalid-example", // Doesn't match {tenant}.conductor.one, so the go sdk default is not changed
+			wantedTenantDomain: "example", // Doesn't match {tenant}.conductor.one, so the go sdk default is not changed
 			wantErr:            false,
 		},
 		{
 			name:               "differentUrlWithHTTP",
 			input:              "insulator.a.different.com",
 			wantedURL:          "https://insulator.a.different.com",
-			wantedTenantDomain: "invalid-example", // Doesn't match {tenant}.conductor.one, so the go sdk default is not changed
+			wantedTenantDomain: "example", // Doesn't match {tenant}.conductor.one, so the go sdk default is not changed
 			wantErr:            false,
 		},
 		{
 			name:               "differentURL",
 			input:              "insulator.a.different.com",
 			wantedURL:          "https://insulator.a.different.com",
-			wantedTenantDomain: "invalid-example", // Doesn't match {tenant}.conductor.one, so the go sdk default is not changed
+			wantedTenantDomain: "example", // Doesn't match {tenant}.conductor.one, so the go sdk default is not changed
 			wantErr:            false,
 		},
 	}
@@ -70,5 +70,82 @@ func TestNormalizeTenantInput(t *testing.T) {
 				t.Errorf("TenantDomain Got = '%v', want '%v'", client.sdkConfiguration.ServerDefaults[0]["tenantDomain"], tt.wantedTenantDomain)
 			}
 		})
+	}
+}
+
+type ClientConfigTest struct {
+	name               string
+	input              string
+	wantedURL          string
+	wantedTenantDomain string
+	tenantOnly         bool
+	wantErr            bool
+}
+
+func TestParseClientID(t *testing.T) {
+	tests := []ClientConfigTest{
+		{name: "base", input: "foobar@insulator.conductor.one/pcc", wantedURL: "https://insulator.conductor.one", wantedTenantDomain: "insulator", wantErr: false, tenantOnly: true},
+		{name: "dev", input: "foobar@c1dev.anthony.dev.ductone.com/pcc", wantedURL: "https://c1dev.anthony.dev.ductone.com", wantedTenantDomain: "", wantErr: false, tenantOnly: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseClientID(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseClientID returned err = '%v', but expected error: '%v'", err, tt.wantErr)
+			}
+			testClientConfig(t, tt, got)
+		})
+	}
+}
+
+func TestCustomTenantOptions(t *testing.T) {
+	tests := []ClientConfigTest{
+		{name: "base", input: "insulator.conductor.one", wantedURL: "https://insulator.conductor.one", wantedTenantDomain: "insulator", wantErr: false, tenantOnly: true},
+		{name: "tenantOnly", input: "insulator", wantedURL: "https://insulator.conductor.one", wantedTenantDomain: "insulator", wantErr: false, tenantOnly: true},
+		{name: "baseWithHTTP", input: "https://insulator.conductor.one", wantedURL: "https://insulator.conductor.one", wantedTenantDomain: "insulator", wantErr: false, tenantOnly: true},
+		{name: "differentURL", input: "https://insulator.a.different.com", wantedURL: "https://insulator.a.different.com", wantedTenantDomain: "", wantErr: false, tenantOnly: false},
+		{name: "differentBase", input: "insulator.a.different.com", wantedURL: "https://insulator.a.different.com", wantedTenantDomain: "", wantErr: false, tenantOnly: false},
+		{name: "devURL", input: "https://c1dev.anthony.dev.ductone.com:2443", wantedURL: "https://c1dev.anthony.dev.ductone.com:2443", wantedTenantDomain: "", wantErr: false, tenantOnly: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := WithTenantCustom(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("WithTenant returned err = '%v', but expected error: '%v'", err, tt.wantErr)
+			}
+			customOptions := &CustomOptions{}
+			got(customOptions)
+			testClientConfig(t, tt, &customOptions.ClientConfig)
+		})
+	}
+}
+
+func testClientConfig(t *testing.T, tt ClientConfigTest, got *ClientConfig) {
+	if got.useWithServer == got.useWithTenant {
+		t.Error("useWithServer and useWithTenant should not be the same")
+	}
+	if got.useWithTenant != tt.tenantOnly {
+		t.Errorf("useWithTenant Got = '%v', want '%v'", got.useWithTenant, tt.tenantOnly)
+	}
+	if got.useWithServer {
+		if got.ServerURL != tt.wantedURL {
+			t.Errorf("ServerURL Got = '%v', want '%v'", got.ServerURL, tt.wantedURL)
+		}
+		if got.Tenant != tt.wantedTenantDomain {
+			t.Errorf("Tenant Got = '%v', want '%v'", got.Tenant, "")
+		}
+	}
+	if got.useWithTenant {
+		if got.Tenant != tt.wantedTenantDomain {
+			t.Errorf("Tenant Got = '%v', want '%v'", got.Tenant, tt.wantedTenantDomain)
+		}
+		if got.ServerURL != "" {
+			t.Errorf("ServerURL Got = '%v', want '%v'", got.ServerURL, "")
+		}
+		if got.GetServerURL() != tt.wantedURL {
+			t.Errorf("GetServerURL Got = '%v', want '%v'", got.GetServerURL(), tt.wantedURL)
+		}
 	}
 }
