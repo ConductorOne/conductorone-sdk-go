5b3546edf50e25a29abb2f02a0c29cbb19ef47c1
Anthony Zhao <anthony.zhao@conductorone.com>
2024-01-04 13:58:59 -0500

    Expanders

--- tools/expander.go
+++ tools/expander.go
@@ -35,32 +35,30 @@ func GetPaths[T any](v *T) []Path {
 	return paths
 }
 
-/* Pass in a function to get the inner object that contains the exapnded Paths
+/* Pass in the list you want to expand with a function to get the inner object that contains the exapnded Paths
  * For example:
  * In the case of `v AppEntitlementWithUserBindings` you would pass in a getter that returns
  * v.AppEntitlementView which is a `*AppEntitlementView`, expects a pointer
  */
+func GetMappedJSONPaths[T, V any](item T, getStructWithPaths func(T) *V) (map[string]int, error) {
+	fn := func(t T) []Path {
+		v := getStructWithPaths(t)
+		return GetPaths(v)
+	}
+	return mapJSONPaths[T](item, fn)
+}
 
-// func GetMappedJSONPaths[T, V any](list []T, getStructWithPaths func(T) *V) (map[string]int, error) {
-// 	if len(list) == 0 {
-// 		return make(map[string]int), nil
-// 	}
-
-// 	return MapJSONPaths[T](list,
-// }
-
-func MapJSONPaths[T any](list []T, getPaths func(T) []Path) (map[string]int, error) {
+func mapJSONPaths[T any](item T, getPaths func(T) []Path) (map[string]int, error) {
 	res := make(map[string]int)
-	for _, item := range list {
-		for _, path := range getPaths(item) {
-			index, err := GetJSONPathIndex(path.Value)
-			if err != nil {
-				return nil, err
-			}
-			if index != -1 {
-				res[path.Key] = index
-			}
+	for _, path := range getPaths(item) {
+		index, err := GetJSONPathIndex(path.Value)
+		if err != nil {
+			return nil, err
+		}
+		if index != -1 {
+			res[path.Key] = index
 		}
 	}
+
 	return res, nil
 }
